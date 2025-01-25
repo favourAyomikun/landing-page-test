2c9f3874433de31863545c6daf68f89b6e83fecb
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-25 14:17:20 +0100

    created the footer component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import dotGlow from "../assets/dotglow.png";
+import Logo from "../assets/Logo.png";
+//
+const Footer = ({ show = true }) => {
+  return (
+    <section
+      className={` ${
+        show ? "pt-16 space-y-14" : ""
+      }  justify-end flex flex-col absolute left-0 h-fit w-full`}
+    >
+      <div
+        className={`${
+          show ? "flex" : "hidden"
+        } justify-center items-center py-9  px-4 sm:px-10`}
+      >
+        <div className="flex md:flex-row flex-col md:w-full lg:w-11/12 xl:w-5/6 justify-between items-center border border-grey15 rounded-lg mx-auto bg-grey11 p-4 md:p-6 lg:p-9 relative overflow-clip">
+          <div className="flex md:text-left text-center space-y-3 md:space-y-0 flex-col w-full md:w-4/5">
+            <h1 className="text-white  font-medium text-2xl sm:text-3xl">
+              Start your financial journey with{" "}
+              <span className="text-col60">YourBank today!</span>
+            </h1>
+            <p className="font-light text-grey70 text-sm sm:text-base lg:text-lg">
+              Ready to take control of your finances? Join YourBank now, and let
+              us help you achieve your financial goals with our tailored
+              solutions and exceptional customer service
+            </p>
+          </div>
+          <div className=" md:w-1/5 w-full flex flex-col items-center pt-4 md:items-end">
+            <button className="bg-col60 px-5 font-medium  w-fit py-2 rounded-l-full rounded-r-full text-grey10">
+              Open Account
+            </button>
+          </div>
+          <img src={dotGlow} alt="gloww" className="absolute top-0 left-0" />
+        </div>
+      </div>
+      <div className="bg-grey11  left-0 w-full flex flex-col items-center px-4 sm:px-10 md:px-20 py-10 space-y-6">
+        <img src={Logo} alt="logo" className="w-24" />
+        <div className="flex space-x-3 text-white lg:text-lg sm:text-base text-sm ">
+          <a href="" className="hover:text-col60">
+            Home
+          </a>
+          <a href="" className="hover:text-col60">
+            Careers
+          </a>
+          <a href="" className="hover:text-col60">
+            About
+          </a>
+          <a href="" className="hover:text-col60">
+            Security
+          </a>
+        </div>
+        <div className="flex flex-wrap sm:flex-nowrap justify-center  w-full  space-x-4 py-9 border border-dashed border-grey20 border-l-0 border-r-0 text-sm md:text-lg">
+          <p>
+            <i className="fa fa-envelope text-col60"></i> hello@skillbridge.com
+          </p>
+          <p>
+            <i className="fa fa-phone text-col60 py-2 sm:py-0"></i> +91 91813 23
+            2309
+          </p>
+          <p>
+            <i className="fa fa-location-dot text-col60 py-2 sm:py-0"></i>{" "}
+            Somewhere in the world
+          </p>
+        </div>
+        <div className="hidden sm:flex text-grey70  font-light justify-between items-center bg-grey10 w-full rounded-l-full rounded-r-full py-3 px-2">
+          <div className="flex space-x-3 text-grey10">
+            <i className="fa-brands fa-facebook bg-col60 rounded-full p-2"></i>
+            <i className="fa-brands fa-twitter bg-col60 rounded-full p-2"></i>
+            <i className="fa-brands fa-linkedin bg-col60 rounded-full py-2 px-[9px]"></i>
+          </div>
+          <p className=" text-sm md:text-base lg:text-lg">
+            YourBank All Rights Reserved
+          </p>
+          <p className="md:px-3">Privacy Policy | Terms of service</p>
+        </div>
+        <div className="flex sm:hidden relative bg-grey10 h-full py-7 rounded-xl w-full border space-y-3 border-grey15 flex-col justify-center items-center text-grey75">
+          <div className="flex space-x-2 text-grey10 absolute -top-4">
+            <i className="fa-brands fa-facebook bg-col60 rounded-full p-2"></i>
+            <i className="fa-brands fa-twitter bg-col60 rounded-full p-2"></i>
+            <i className="fa-brands fa-linkedin bg-col60 rounded-full py-2 px-[9px]"></i>
+          </div>
+          <p className=" text-sm">YourBank All Rights Reserved</p>
+          <p className="text-sm">Privacy Policy | Terms of service</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
---
+++ app/landing-page/page.jsx
@@ -0,0 +1,111 @@
+// import React from "react";
+// import HeroImg from "../assets/Container.png";
+// import Abstract from "../assets/Abstract.png";
+// import Button from "./Button";
+// import UseCaseSect from "./Sects/UseCaseSect";
+// import Features from "./Sects/Features";
+// import Faq from "./Sects/Faq";
+// import IndividualBtn from "./IndividualBtn";
+// import Testimonial from "./Sects/Testimonial";
+// import Footer from "./Footer";
+
+// const Home = () => {
+//   return (
+//     <section className="w-full">
+//       {/* Hero Section */}
+//       <section className="flex px-2 md:px-0 flex-col md:flex-row font-lexend justify-around w-full py-8 items-start">
+//         <div className="py-3 flex items-center md:items-start flex-col text-white md:w-1/2 w-full space-y-3">
+//           <span className="px-3 py-1 text-wh99 shadow-md bg-grey15 text-xs sm:text-sm lg:text-lg rounded-l-full rounded-r-full w-fit text-center">
+//             No LLC Required,No Credit Check
+//           </span>
+//           <h1 className="text-center md:text-left text-3xl sm:text-4xl lg:text-5xl font-medium">
+//             Welcome to YourBank
+//           </h1>
+//           <h1 className="text-3xl text-center md:text-left sm:text-4xl lg:text-5xl font-medium">
+//             Empowering Your{" "}
+//             <span className="text-col60">Financial Journey</span>
+//           </h1>
+//           <p className="font-light md:text-left text-grey70 text-center text-sm sm:text-base lg:text-lg">
+//             At YourBank, our mission is to provide comprehensive banking
+//             solutions that empower individuals and businesses to achieve their
+//             financial goals. We are committed to delivering personalized and
+//             innovative services that prioritize our customers' needs.
+//           </p>
+//           <Button text="Open Account" />
+//         </div>
+//         <div className="md:w-1/2 relative w-full py-4 md:py-0 flex justify-center items-center">
+//           <img
+//             src={HeroImg}
+//             className="md:w-3/4 w-full"
+//             alt="Hero section image"
+//           />
+//           <img
+//             src={Abstract}
+//             alt="Abstract design"
+//             className="absolute top-5 -right-5 md:right-0 -z-10"
+//           />
+//         </div>
+//       </section>
+//       {/* Our products */}
+//       <section className="flex flex-col py-20 space-y-5">
+//         <div className="flex flex-col md:flex-row justify-between items-end md:space-y-0 space-y-3">
+//           <div className="flex flex-col md:w-1/2 w-full">
+//             <h1 className="font-medium text-center md:text-left text-3xl sm:text-4xl lg:text-5xl">
+//               Our <span className="text-col60">Products</span>
+//             </h1>
+//             <p className="text-grey70 text-center md:text-left text-light text-base">
+//               Discover a range of comprehensive and customizable banking
+//               products at YourBank, designed to suit your unique financial needs
+//               and aspirations
+//             </p>
+//           </div>
+//           <div className="flex w-fit mx-auto md:1/2 lg:w-1/3 xl:w-[29%] text-sm py-2 px-3 bg-grey11  border-2 border-grey20 rounded-l-full rounded-r-full justify-between">
+//             <IndividualBtn />
+//           </div>
+//         </div>
+//         <div className="flex flex-col md:flex-row text-white items-center justify-around pt-8 md:space-x-5">
+//           <div className="flex space-y-3 w-full md:py-0 py-3 md:w-1/3 flex-col justify-between items-center">
+//             <i>icon</i>
+//             <h3 className="text-2xl">Checking Accounts</h3>
+//             <p className="text-grey70 text-center font-light text-lg">
+//               Enjoy easy and convenient access to your funds with our range of
+//               checking account options. Benefit from features such as online and
+//               mobile banking, debit cards, and free ATM access.
+//             </p>
+//           </div>
+//           <div className="flex space-y-3 border-t border-b py-4 md:border-t-0 md:border-b-0 md:py-0 md:border-l md:px-3 md:border-r border-grey20 w-full md:w-1/3 flex-col justify-between items-center">
+//             <i>icon</i>
+//             <h3 className="text-2xl text-center">Loan and Mortgages</h3>
+//             <p className="text-grey70 text-center font-light text-lg">
+//               Build your savings with our competitive interest rates and
+//               flexible savings account options. Whether you're saving for a
+//               specific goal or want to grow your wealth over time, we have the
+//               right account for you.
+//             </p>
+//           </div>
+//           <div className="flex space-y-3 w-full md:py-0 py-3 md:w-1/3 flex-col justify-between items-center">
+//             <i>icon</i>
+//             <h3 className="text-2xl">Savings Accounts</h3>
+//             <p className="text-grey70 text-center font-light text-lg">
+//               Build your savings with our competitive interest rates and
+//               flexible savings account options. Whether you're saving for a
+//               specific goal or want to grow your wealth over time, we have the
+//               right account for you.
+//             </p>
+//           </div>
+//         </div>
+//       </section>
+//       {/* Use Cases */}
+//       <UseCaseSect />
+//       {/* Features */}
+//       <Features />
+//       {/* FAQ */}
+//       <Faq />
+//       {/* testimony */}
+//       <Testimonial />
+//       {/* footer */}
+//       <Footer />
+//     </section>
+//   );
+// };
+// export default Home;
